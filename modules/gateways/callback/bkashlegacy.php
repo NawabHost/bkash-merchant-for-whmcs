d26bc0d51a08d452c00912bb1426106a1da9a780
Aminul Islam <me@aminul.net>
2021-05-27 08:45:52 +0600

    fix type casting

--- modules/gateways/callback/bkashlegacy.php
+++ modules/gateways/callback/bkashlegacy.php
@@ -157,8 +157,8 @@ class bKashLegacy
      */
     private function setCurrency()
     {
-        $this->gatewayCurrency  = (int)$this->gatewayParams['convertto'];
-        $this->customerCurrency = \WHMCS\Database\Capsule::table('tblclients')
+        $this->gatewayCurrency  = (int) $this->gatewayParams['convertto'];
+        $this->customerCurrency = (int) \WHMCS\Database\Capsule::table('tblclients')
             ->where('id', '=', $this->invoice['userid'])
             ->value('currency');
 
